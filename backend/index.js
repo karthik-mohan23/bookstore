33988f70bfdbd42d3dcbb35410370c42deec7d21
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-09-01 19:49:00 +0530

    create our first route

--- backend/index.js
+++ backend/index.js
@@ -4,4 +4,9 @@ const { PORT } = require("./config");
 
 const app = express();
 
+// routes
+app.get("/", (req, res) => {
+  return res.status(200).send("Welcome to Bookstore app");
+});
+
 app.listen(PORT, () => "Server up and running");
